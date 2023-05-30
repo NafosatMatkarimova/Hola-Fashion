42fc0c56134ac2865478a2a255971871d950f096
nafosat424@gmail.com <nafosat@gmail.com>
2023-05-30 14:36:40 +0500

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+
+const Home = () => {
+    return(
+        <ReactFragment>
+         <Header/>
+        </ReactFragment>
+    )
+}
+export default Home;
